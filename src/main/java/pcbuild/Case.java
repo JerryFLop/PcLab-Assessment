caaccefa9f97563c5dab0d2541e8345f896f8b33
JerryFlop <JParedes@wil.yearup.org>
2023-04-07 15:37:20 -0400

    commit

--- src/main/java/pcbuild/Case.java
+++ src/main/java/pcbuild/Case.java
@@ -4,9 +4,15 @@ public class Case {
    private String model;
     private String manufacturer;
     private String powerSupply;
-    private Dimensions dimensions;
+    private  Dimension dimension;
 
 
+    public Case(String model,String manufacturer,String powerSupply,Dimension dimension) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.powerSupply = powerSupply;
+        this.dimension = dimension;
+    }
 
 
 
